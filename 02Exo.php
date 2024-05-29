c02309a4a86406dcecf6c73ab3c6fcd5395ae3c5
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-29 15:27:39 +0200

    MAJ

--- 02Exo.php
+++ 02Exo.php
@@ -1,7 +1,7 @@
 <h1>Exo 2</h1>
 
 <p>Soit le tableau suivant :
-$capitales = [France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome"];
+$capitales = ["France"=>"Paris","Allemagne"=>"Berlin","USA"=>"Washington","Italie"=>"Rome"];
 Réaliser un algorithme permettant d’afficher le tableau HTML suivant (notez que le nom du pays
 s’affichera en majuscule et que le tableau est trié par ordre alphabétique du nom de pays) grâce à une fonction personnalisée.
 Vous devrez appeler la fonction comme suit : afficherTableHTML($capitales);</p>
@@ -9,4 +9,13 @@ Vous devrez appeler la fonction comme suit : afficherTableHTML($capitales);</p>
 <H2>Le résultat</H2>
 
 <?PHP
+
+$capitales = [
+    "France"=>"Paris",
+    "Allemagne"=>"Berlin",
+    "USA"=>"Washington",
+    "Italie"=>"Rome"];
+
+$capitales = implode("<br>", $capitales);
+print_r($capitales) ;
 
