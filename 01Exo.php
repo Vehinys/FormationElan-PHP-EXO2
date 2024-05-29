be8b8e914773f047c6cd20d227f13a9111adcea1
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-29 15:11:20 +0200

    EXO 1

--- 01Exo.php
+++ 01Exo.php
@@ -1,10 +1,28 @@
 <h1>Exo 1</h1>
 
-<p>Créer une fonction personnalisée convertirMajRouge() permettant de transformer une chaîne de caractère passée en argument en majuscules et en rouge.
+<p>
+Créer une fonction personnalisée convertirMajRouge() permettant de transformer 
+une chaîne de caractère passée en argument en majuscules et en rouge.
 Vous devrez appeler la fonction comme suit : convertirMajRouge($texte) ;</p>
 
 <H2>Le résultat</H2>
 
+<style>
+    .red {
+      color : red;
+      }
+</style>
+
 <?PHP
 
+$texte = "Mon texte en paramètre";
 
+function convertirMajRouge($texte) {
+      $result = mb_strtoupper($texte); 
+      $result = "<p class = 'red'>".$result."</p>";
+      return $result;
+}
+
+echo convertirMajRouge($texte);
+
+?>
