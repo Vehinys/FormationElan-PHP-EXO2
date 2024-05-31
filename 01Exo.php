55e76f3303f7cfd417f9ca891339490bd5893911
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-05-31 10:30:41 +0200

    MAJ COMMENTAIRE

--- 01Exo.php
+++ 01Exo.php
@@ -12,12 +12,10 @@
 
 $texte = "Mon texte en paramètre";                    /* Texte predifini */
 
-function convertirMajRouge($texte) {                  /* Texte predifini */
-      $result = mb_strtoupper($texte);                /* Texte predifini */
+function convertirMajRouge($texte) {                  /* function au nom de convertirMajRouge qui reprend $texte */
+      $result = mb_strtoupper($texte);                /* $result = text en majuscule et qui prend aussi en compte les Accents qui reprend $texte */
       $result = "<p class = 'red'>".$result."</p>";   /* §result = au paragraphe on rajoute la couleur rouge qui se concaténate sur $result */
       return $result;                                 /* Retourner le resultat de $Result */
 }
 
-echo convertirMajRouge($texte);
-
-?>
+echo convertirMajRouge($texte);                       /* Afficher la function convertirMajRouge($texte)*/
