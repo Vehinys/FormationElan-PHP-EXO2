a072fa014eabba61bdd80b76e1594adc6476fc38
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-02 18:02:47 +0200

    REPETITION

--- 08Exo.php
+++ 08Exo.php
@@ -4,13 +4,15 @@
 
 <?php
 
-$url = "my.mobirise.com/data/userpic/764.jpg" ;
+$url = " my.mobirise.com/data/userpic/764.jpg " ;
 $nombreDeFois = 4;
 
 function repeterImage($url, $nombreDeFois) {
+
     for ($i = 0; $i < $nombreDeFois; $i++) {
+        
         echo "<img src='$url'>";
     }
 }
 
-repeterImage("http://my.mobirise.com/data/userpic/764.jpg", 4);
+repeterImage("http://my.mobirise.com/data/userpic/764.jpg", $nombreDeFois);
