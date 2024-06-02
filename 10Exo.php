c5c24750dbe53f47775586b628597036d8b4c0af
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-06-02 18:45:17 +0200

    RAPPEL

---
+++ 10Exo.php
@@ -0,0 +1,38 @@
+<?php
+
+$nomsInput = ["Nom", "Prénom","Email","Ville","Sexe"];
+
+
+function afficherInput($nomsInput) {
+
+    foreach ($nomsInput as $nom) {
+
+        echo '<label>' . $nom . ' :</label><br>';
+        echo '<input type="text"><br>';
+
+    }
+    echo "<br>";
+}
+
+echo afficherInput($nomsInput);
+
+
+$elements = ["Développeur logiciel", "Designer Web", "Integrateur Web", "Chef de projet"];
+
+
+function alimenterListeDeroulante($elements) {
+
+    echo '<select>';
+
+    foreach ($elements as $civil) {
+        echo '<option>' . $civil . '</option>';
+    }
+    
+    echo '</select>';
+}
+
+echo alimenterListeDeroulante($elements);
+
+echo "<br><br>";
+
+echo '<input type="submit">';
